724b4946c37719a65d47765c21848858eda62b97
Steve Kinney <hello@stevekinney.net>
2023-10-26 21:24:40 -0600

    Remove implementations

--- README.md
+++ README.md
@@ -17,3 +17,22 @@ The SQLite database is not checked into version control, but you can create it q
 ```
 npx run database
 ```
+
+## Running the Payments Server
+
+There is a simple little server that we can spin up or take down. When we're using Temporal, the workflow will retry to process the payment until the server come back up.
+
+You can start the server using the following script:
+
+```
+npm run payments
+```
+
+## Two Implementations
+
+1. `src/lib/_with-database.ts` is a naïve implementation. It's what we have in `src/lib/index.ts` by default. This exists in order to demonstrate how the application works without Temporal.
+2. `src/lib/_with-temporal.ts` supports a _really_ simple workflow. There is definitely a lot more we could do to improve the workflow, but this is enough to give us a sense of what it's like to work with Temporal.
+
+## Completed Example
+
+The completed example can be found in `src/lib/temporal/_complete-workflow.ts`.
--- src/lib/temporal/workflows.ts
+++ src/lib/temporal/workflows.ts
@@ -1,44 +1,11 @@
-import { condition, defineSignal, proxyActivities, setHandler, sleep } from '@temporalio/workflow';
-import type * as activities from './activities';
-import OrderStatus from '../utilities/status';
+import { defineSignal } from '@temporalio/workflow';
+import type OrderStatus from '$lib/utilities/status';
 
-const { createOrder, updateOrder, processPayment, sendFollowUp } = proxyActivities<
-	typeof activities
->({
-	startToCloseTimeout: '1 minute'
-});
+// import type * as activities from './activities';
 
 export const resturantConfirmation = defineSignal('confirm');
 
 export const updateStatus = defineSignal<[OrderStatus]>('updateStatus');
 
-export async function kickOffFoodOrderWorkflow(items: Omit<Item, 'id'>[], id: string) {
-	const order = await createOrder(items, id);
-	const orderId = order.id;
-
-	let confirmed = false;
-
-	setHandler(resturantConfirmation, () => {
-		confirmed = true;
-	});
-
-	await updateOrder(orderId, OrderStatus.ProcessingPayment);
-
-	await processPayment(orderId);
-
-	await updateOrder(orderId, OrderStatus.WaitingForRestaurantConfirmation);
-
-	await condition(() => confirmed);
-
-	await updateOrder(orderId, OrderStatus.Confirmed);
-
-	await sleep('1 minute');
-
-	await updateOrder(orderId, OrderStatus.ReadyForPickup);
-	await updateOrder(orderId, OrderStatus.EnRoute);
-	await updateOrder(orderId, OrderStatus.Delivered);
-
-	await sleep('1 day');
-
-	await sendFollowUp();
-}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function kickOffFoodOrderWorkflow(items: Omit<Item, 'id'>[], id: string) {}
