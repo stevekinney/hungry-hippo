352be5924c1de91426c5c322ef060c82b469c931
Steve Kinney <hello@stevekinney.net>
2023-10-27 10:02:55 -0600

    Use Temporal

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,24 +1,31 @@
-import axios from 'axios';
 import * as db from './database';
-import OrderStatus from './utilities/status';
+import temporal from './temporal';
+import { kickOffFoodOrderWorkflow, updateStatus } from './temporal/workflows';
+import { generateId } from './utilities/id';
 
+import type OrderStatus from './utilities/status';
+
+// We *could* query Temporal all of the workflows that have to do with orders.
 export const getOrder = (id: string) => {
 	return db.getOrder(id);
 };
 
+// We *could* query Temporal for a given workflow for an order.
 export const getOrders = () => {
 	return db.getOrders();
 };
 
-export const createOrder = (items: Omit<Item, 'id'>[]) => {
-	return db.createOrder(items);
+export const createOrder = async (items: Omit<Item, 'id'>[], id: string = generateId()) => {
+	await temporal.workflow.start(kickOffFoodOrderWorkflow, {
+		args: [items, id],
+		taskQueue: 'food-order',
+		workflowId: `order-${id}`
+	});
+
+	return { id };
 };
 
-export const updateOrderStatus = async (id: string, status: OrderStatus) => {
-	if (status === OrderStatus.ProcessingPayment) {
-		// This is going to go poorly if the payment processor is down.
-		await axios.post(`http://localhost:3333/charge/${id}`);
-	}
-
-	return db.updateOrderStatus(id, status);
+export const updateOrderStatus = (id: string, status: OrderStatus) => {
+	const workflow = temporal.workflow.getHandle(`order-${id}`);
+	workflow.signal(updateStatus, status);
 };
--- src/lib/temporal/workflows.ts
+++ src/lib/temporal/workflows.ts
@@ -1,7 +1,72 @@
-import type OrderStatus from '$lib/utilities/status';
-import { defineSignal } from '@temporalio/workflow';
+import {
+	condition,
+	defineQuery,
+	defineSignal,
+	proxyActivities,
+	setHandler,
+	sleep
+} from '@temporalio/workflow';
 
+import OrderStatus, { formatStatus } from '../utilities/status';
+import type * as activities from './activities';
+
+const { createOrder, updateOrder, processPayment, sendFollowUp } = proxyActivities<
+	typeof activities
+>({
+	startToCloseTimeout: '1 minute'
+});
+
+export const resturantConfirmation = defineSignal('confirm');
 export const updateStatus = defineSignal<[OrderStatus]>('updateStatus');
+export const getStatus = defineQuery('getStatus');
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function kickOffFoodOrderWorkflow(items: Omit<Item, 'id'>[], id: string) {}
+export async function kickOffFoodOrderWorkflow(items: Omit<Item, 'id'>[], id: string) {
+	const order = await createOrder(items, id);
+
+	let status: OrderStatus = order.status;
+	let confirmed = false;
+	let paymentVeried = false;
+
+	// We can use signals to communicate with the workflow.
+	setHandler(resturantConfirmation, () => {
+		confirmed = true;
+	});
+
+	// Signals are added to the history, so we can use them to update the workflow.
+	setHandler(updateStatus, async (newStatus: OrderStatus) => {
+		status = await updateOrder(id, newStatus);
+	});
+
+	// We can use queries to get information from the workflow.
+	setHandler(getStatus, () => {
+		return formatStatus(status);
+	});
+
+	status = await updateOrder(id, OrderStatus.ProcessingPayment);
+
+	await processPayment(id);
+
+	paymentVeried = true;
+
+	// Pause and wait for this expression to be truthy.
+	await condition(() => paymentVeried);
+
+	status = await updateOrder(id, OrderStatus.WaitingForRestaurantConfirmation);
+
+	await condition(() => confirmed);
+
+	status = await updateOrder(id, OrderStatus.Confirmed);
+
+	await sleep('1 minute');
+
+	// In a perfect world, we'd implement more logic here.
+	// But, I think you're starting to get the idea.
+	status = await updateOrder(id, OrderStatus.ReadyForPickup);
+	status = await updateOrder(id, OrderStatus.EnRoute);
+	status = await updateOrder(id, OrderStatus.Delivered);
+
+	// Think abouot all of the code you're *not* writing.
+	await sleep('1 day');
+
+	await sendFollowUp();
+}
