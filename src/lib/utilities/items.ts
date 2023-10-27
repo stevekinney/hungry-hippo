b2ecae20357c5213c9bdeb38a52aa6917a9c5964
Steve Kinney <hello@stevekinney.net>
2023-10-27 05:49:16 -0600

    Fix svelte-check warning

--- src/lib/utilities/items.ts
+++ src/lib/utilities/items.ts
@@ -1,4 +1,4 @@
-export const items: Item[] = [
+export const items: Readonly<Item[]> = [
 	{ id: 1, name: 'Vegan Ham Burrito', price: 15, quantity: 0 },
 	{ id: 2, name: 'Oatmeal Nachos', price: 5, quantity: 0 },
 	{ id: 3, name: 'Chocolate Chip Cookie Fajitas', price: 12, quantity: 0 }
